d86a198e7dc45663cc8ce2e31debdaabe23dd885
nimesh_03 <nimesh_03@yahoo.com>
2018-05-23 00:32:05 +0545

    precompile 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.digest = true
+   # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = true
+
 end
