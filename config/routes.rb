68c958069a4bacda513c4100b73ec3f02d108ef4
nimesh_03 <nimesh_03@yahoo.com>
2018-05-23 01:59:07 +0545

    precompile 10

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root "pages#index"
   resources :microposts
   resources :users
-  resources :pages
   get "signup", to: "users#new"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
