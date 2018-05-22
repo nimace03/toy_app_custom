54754fe10718430a4bdb5276cb18bdf76c6b1de1
nimesh_03 <nimesh_03@yahoo.com>
2018-05-23 01:56:57 +0545

    precompile 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root "pages#index"
   resources :microposts
   resources :users
   resources :pages
-  get 'signup', to: 'users#new'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
